0ad60a812abceb5e68a4c3f5eaf866ff98157800
Lars Grüter <lagru@mailbox.org>
2025-10-24 14:23:46 +0200

    Avoid early exit in assert-unchanged.sh
    and also show git status as a summary on failure.

--- .github/scripts/assert-unchanged.sh
+++ .github/scripts/assert-unchanged.sh
@@ -9,18 +9,19 @@ CHECK_DIR=$1
 
 # Find untracked files
 UNTRACKED=$(git ls-files --others --exclude-standard "$CHECK_DIR")
-# and display their content by comparing with '/dev/null'
-echo "$UNTRACKED" | xargs -I _ git --no-pager diff --no-index /dev/null _
+
+# Display diff of each untracked file by comparing with '/dev/null'
+# Hide exit code of `git diff` to avoid early exit due to `set -e`
+echo "$UNTRACKED" | xargs -I _ git --no-pager diff /dev/null _ || true
 
 # Display changes in tracked files and capture non-zero exit code if so
-set +e
-git diff --exit-code HEAD "$CHECK_DIR"
+git diff --exit-code HEAD "$CHECK_DIR" || true
 GIT_DIFF_HEAD_EXIT_CODE=$?
-set -e
 
 # Display changes in tracked files and capture exit status
 if [ $GIT_DIFF_HEAD_EXIT_CODE -ne 0 ] ||  [ -n "$UNTRACKED" ]; then
   echo "::error::Uncommited changes in directory '$CHECK_DIR'"
+  git status --porcelain
   exit 1
 else
   echo "::notice::No Uncommited changes, directory '$CHECK_DIR' is clean"
