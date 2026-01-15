5514ac3d25aba89451907bffcf1b13148f2ff2cb
Lars Grüter <lagru@mailbox.org>
2026-01-15 08:20:35 +0100

    Sort src lines in log messages "naturally" (#119)
    * Sort src lines in log messages "naturally"
    
    * Sync stubs with source code changes
    
    * Fix return type

--- src/docstub-stubs/_utils.pyi
+++ src/docstub-stubs/_utils.pyi
@@ -5,6 +5,7 @@ import re
 from collections.abc import Callable, Hashable, Mapping, Sequence
 from functools import lru_cache, wraps
 from pathlib import Path
+from typing import Any
 from zlib import crc32
 
 def accumulate_qualname(qualname: str, *, start_right: bool = ...) -> None: ...
@@ -20,3 +21,7 @@ def update_with_add_values(
 
 class DocstubError(Exception):
     pass
+
+_regex_digit: re.Pattern
+
+def naive_natsort_key(item: Any) -> tuple[str | int, ...]: ...
